d116fef8b12d45395a31c5de99b46657beeebddf
monupndt921 <monupndt921@gmail.com>
2026-07-03 23:35:11 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    ChatJoinRequestHandler,
+    ContextTypes,
+)
+
+BOT_TOKEN = "8821858157:AAHCDZBqmHtUC1PvJJw4gD4XPQzdRUsGYoU"
+
+async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        await update.chat_join_request.approve()
+        print(f"Approved: {update.chat_join_request.from_user.id}")
+    except Exception as e:
+        print(e)
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(ChatJoinRequestHandler(approve))
+
+print("Auto Approve Bot Running...")
+app.run_polling()
